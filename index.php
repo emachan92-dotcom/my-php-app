a84710edcf1425043de2e4a2c3d7a3af43e11f37
emachan92-dotcom <emachan92@gmail.com>
2025-10-30 10:53:32 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
-<?php session_start(); ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>ログイン</title>
-</head>
-<body>
-<h2>ログインフォーム</h2>
-<form method="post" action="login.php">
-    Email: <input type="email" name="email" required><br>
-    名前: <input type="text" name="name" required><br>
-    <button type="submit">ログイン</button>
-</form>
-<p><a href="register.php">新規登録はこちら</a></p>
-</body>
-</html>
-
+<?php
+header("Location: login.php");
+exit;
